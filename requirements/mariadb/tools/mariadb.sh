56b45a667ec120c0547af4f59dd7b4fcae5678ce
Jean Carlen <jeancarlen@macbook-pro-de-jean.home>
2023-06-27 10:40:52 +0200

    push for vm 2

--- requirements/mariadb/tools/mariadb.sh
+++ requirements/mariadb/tools/mariadb.sh
@@ -1,29 +1,40 @@
 #!/bin/sh
 
+# Start the MySQL service
 mysql_install_db
+service mysql start
 
-/etc/init.d/mysql start
+# Wait for MySQL to start
+sleep 5
 
-#Check if the database exists
-
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-then 
-	echo "Database already exists"
+# Check if the database exists
+if [ -d "/var/lib/mysql/$SQL_DATABASE" ]; then
+    echo "Database already exists"
 else
-	echo "CREATE DATABASE IF NOT EXISTS '${SQL_DATABASE}';" | mysql -uroot
-	echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED by '${SQL_PASSWORD}' ;" | mysql -uroot
-	echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD '${SQL_ROOT_PASSWORD}';"| mysql -uroot
-	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}' WITH GRANT OPTION;" | mysql -uroot
-	# echo "DELETE FROM mysql.user WHERE user != 'root' AND user != 'mariadb.sys' OR (user = 'root' AND host != 'localhost');"| mysql -uroot
-	echo "GRANT ALL PRIVILEGES ON '${SQL_NAME}'.* TO '${SQL_USER}' @'%';"| mysql -uroot
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"| mysql -uroot
-	echo "FLUSH PRIVILEGES;" | mysql -uroot
-
-	#Import database in the mysql command line
-	#mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-
+    # Create the database and user
+    echo "Creating database..."
+    echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
+    
+    echo "Creating user..."
+    echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
+    
+    echo "Granting privileges..."
+    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}' WITH GRANT OPTION;" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
+    echo "GRANT ALL PRIVILEGES ON ${SQL_HOSTNAME}.* TO '${SQL_USER}'@'%';" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
+    
+    echo "Flushing privileges..."
+    echo "FLUSH PRIVILEGES;" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
+    
+    echo "Changing root user password..."
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
+    
+    echo "Granting root privileges..."
+    echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -uroot -p'${SQL_ROOT_PASSWORD}'
 fi
 
-/etc/init.d/mysql stop
+echo "we are here"
+
+# Stop the MySQL service
+service mysql stop
 
 exec "$@"
--- requirements/wordpress/tools/create_wordpress.sh
+++ requirements/wordpress/tools/create_wordpress.sh
@@ -16,10 +16,10 @@ else
 	rm -rf wordpress
 
 	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$SQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 fi
 
